d5639daeaf21ea4ea2415e1547e94957db310e50
VladimirS0l <vbnm1993@yandex.ru>
2022-11-14 23:14:27 +0300

    Cat and Man1.1

--- JavaHW5/Cat.java
+++ JavaHW5/Cat.java
@@ -9,15 +9,4 @@ public class Cat {
     public String toString() {
         return String.format("n:%s c:%s t:%s", name, color, type);
     }
-
-    // @Override
-    // public boolean equals(Object o) {
-    //     var t = (WORKER) o;
-    //     return id == t.id && firstName == t.firstName;
-    // }
-
-    // @Override
-    // public int hashCode() {
-    //     return id;
-    // }
 }
--- JavaHW5/Man.java
+++ JavaHW5/Man.java
@@ -9,15 +9,4 @@ public class Man {
     public String toString() {
         return String.format("fn:%s ln:%s doc:%s", firstName, lastName, name_animal);
     }
-
-    // @Override
-    // public boolean equals(Object o) {
-    //     var t = (WORKER) o;
-    //     return id == t.id && firstName == t.firstName;
-    // }
-
-    // @Override
-    // public int hashCode() {
-    //     return id;
-    // }
 }
